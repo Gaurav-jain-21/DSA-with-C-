2b3a938edd1f91a00ecfd38ee6a6beaa6864f95e
Gaurav Kumar Borad <gjain7764@gmail.com>
2025-10-20 16:09:50 +0530

    this is kind a pattern

--- pattern.cpp
+++ pattern.cpp
@@ -1,14 +1,49 @@
 #include<iostream>
 using namespace std;
 int main() {
-    cout<<"Enter any number : ";
-    int c;
-    cin>>c;
-    for (int i=1; i<=c; i++) {
-        for (int j=1; j<=i; j++) {
+    //make simple pattern
+    cout<<"Enter the number to make a patttern ";
+    int a;
+    cin>>a;
+    for (int i =1; i<=a; i++) {
+        for (int j =1; j<=i; j++) {
             cout<<"*";
         }
         cout<<endl;
     }
-
+    //make a deceanding order pattern
+    cout<<"enter the again a number ";
+    int b;
+    cin>>b;
+    for (int i=1; i<=b; i++) {
+        for (int j=1; j<=b-i+1; j++) {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    //make a pattern with space and then stars
+    cout<< "enter the number ";
+    int c;
+    cin>>c;
+    for (int i=1;i<=c; i++) {
+        for (int j=1; j<=c-i; j++) {
+            cout<<" ";
+        }
+        for (int k=1; k<=i; k++) {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    cout<<"enter the number: ";
+    int d;
+    cin>>d;
+    for (int i=1; i<=d; i++) {
+        for (int j=1;j<=i-1; j++) {
+            cout<<" ";
+        }
+        for (int k =1; k<=d-i+1; k++) {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
 }
