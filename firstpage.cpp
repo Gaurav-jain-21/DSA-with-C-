48b89f6799229eebc7040a62d351641121e12d9d
Gaurav Kumar Borad <gjain7764@gmail.com>
2025-11-08 01:03:00 +0530

    this is the first page of my DSA playlist

---
+++ firstpage.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main() {
+    // int a = 123;
+    // cout<<a<<endl;
+    // char b= 'v';
+    // cout<<sizeof(b)<<endl;
+    // cout<<b<<endl;
+    // bool bl= false;
+    // cout<<bl<<endl;
+    // float c= 3.175324;
+    // cout<<c<<endl;
+    // double d= 123.456;
+    // cout<<d<<endl;
+    // int size= sizeof(d);
+    // cout<<size<<endl;
+    // cout<<"world hello"<<endl;
+    int a= 'a';
+    cout<<a<<endl;
+    char ch= 98;
+    cout<<ch;
+}
