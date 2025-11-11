7855e15162a3a12fb95f2b6748a5e88038f7eb49
Gaurav Kumar Borad <gjain7764@gmail.com>
2025-11-11 23:49:08 +0530

    BIT Wise operator

---
+++ bitwiseOperator.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int a = 4;
+    int b= 6;
+    cout << (a&b);
+    cout<<(a|b);
+    cout<<(~a);
+    cout<<(a^b);
+    cout<<endl;
+    cout<<(17>>1);
+    cout<<endl;
+    cout<<(12>>2);
+    cout<<endl;
+    cout<<(19<<1);
+    cout<<endl;
+    cout<<(21<<2);
+    cout<<endl;
+}
