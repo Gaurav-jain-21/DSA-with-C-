c154ead0d0200700417c41e80ca9f971c23097b0
Gaurav Kumar Borad <gjain7764@gmail.com>
2025-12-06 14:09:58 +0530

    this is the DSA series

--- firstpage.cpp
+++ firstpage.cpp
@@ -1,22 +1,66 @@
 #include<iostream>
 using namespace std;
+void camp(int a,int b) {
+    if (a>b) {
+        cout<<"a  is greater than ";
+    }
+    else if (a<b) {
+        cout<<"b is greater than a";
+
+    }
+    else {
+        cout<<"every number is equal";
+    }
+}
+
 int main() {
-    // int a = 123;
-    // cout<<a<<endl;
-    // char b= 'v';
-    // cout<<sizeof(b)<<endl;
-    // cout<<b<<endl;
-    // bool bl= false;
-    // cout<<bl<<endl;
-    // float c= 3.175324;
-    // cout<<c<<endl;
-    // double d= 123.456;
-    // cout<<d<<endl;
-    // int size= sizeof(d);
-    // cout<<size<<endl;
-    // cout<<"world hello"<<endl;
-    int a= 'a';
-    cout<<a<<endl;
-    char ch= 98;
-    cout<<ch;
+    // char ch = 97;
+    // cout<<ch;
+    // int a= 2;
+    // int b= a+1;
+    // if ((a=3)==b) {
+    //     cout<<a;
+    //
+    // }
+    // else {
+    //     cout<<a+1;
+    // }
+    // int a= 9;
+    // if (a==9) {
+    //     cout<< "Niney";
+    //
+    // }
+    // if (a>0) {
+    //     cout<<"positive";
+    // }
+    // else {
+    //     cout<<"negative";
+    // }
+    // int a= 24;
+    // if (a>20) {
+    //     cout<<"Love";
+    // }
+    // else if (a==24) {
+    //     cout<<"Lovely";
+    // }
+    // else {
+    //     cout<<"babbar";
+    // }
+    // cout<<a;
+    cout<<"enter the number to print the pattern ";
+    int a;
+    cin>>a;
+    int i=1;
+    while (i<=a) {
+        int j= 1;
+        while (j<=a) {
+            cout<<i;
+            j++;
+        }
+        i++;
+        cout<<endl;
+    }
+
+
+
 }
