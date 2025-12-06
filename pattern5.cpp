4c936450dd30d44d9369830687dc73b68f4efcbc
Gaurav Kumar Borad <gjain7764@gmail.com>
2025-12-06 14:41:59 +0530

    patterns

--- pattern5.cpp
+++ pattern5.cpp
@@ -25,8 +25,53 @@ void revnumber1(int a) {
         cout<<endl;
     }
 }
+void seqnumber(int a) {
+    int i=1;
+    int count=1;
+    while (i<=a) {
+        int j=1;
+        while (j<=a) {
+            cout<<count<<" ";
+            count++;
+            j++;
+        }
+        i++;
+        cout<<endl;
+    }
+}
+void tripattern(int a) {
+    int i= 1;
+    int count =1;
+    while (i<=a) {
+        int j=1;
+        while (j<=i) {
+            cout<<count<<" ";
+            count++;
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+}
+void tripattern2(int a) {
+    int i=1;
+    int count=1;
+    while (i<=a) {
+        int j=1;
+        count = i;
+        while (j<=i) {
+            cout<<count<<" ";
+            count++;
+            j++;
+        }
+        i++;
+        cout<<endl;
+    }
+}
 int main() {
-    revnumber1(4);
+    // revnumber1(4);
+    // seqnumber(3);
+    tripattern2(5);
 
 
 }
