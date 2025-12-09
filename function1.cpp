c86072a7015b87727a7e99ad580fc088d4f6b6f5
Gaurav Kumar Borad <gjain7764@gmail.com>
2025-12-09 12:57:06 +0530

    this is for switch case and functions

--- function1.cpp
+++ function1.cpp
@@ -21,7 +21,38 @@ void switchcase() {
         default : cout<<"Error"<<endl;
     }
 }
+int power(int a, int b) {
+    int ans= 1;
+    for (int i =1;i<=b; i++) {
+        ans= ans*a;
+    }
+    return ans;
+}
+bool isEven(int a) {
+    if (a&1) {
+        return 0;
+    }
+    return 1;
+}
+int factorial(int a) {
+    int ans=1;
+    for (int i= 1; i<=a ; i++) {
+        ans= ans*i;
+    }
+    return ans;
+}
+int nCr(int n, int r) {
+    int ans=((factorial(n)/(factorial(r)*factorial(n-r))));
+    return ans;
+}
+bool isPrime(int n) {
+    for (int i=2 ; i<n; i++) {
+        if (n%i==0) {
+            return 0;
+        }
+    }
+    return 1;
+}
 int main() {
-    switchcase();
-
+    cout<<isPrime(11);
 }
