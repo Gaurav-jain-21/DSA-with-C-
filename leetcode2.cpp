adc5c29ca53b726de05002b4775d7f04c4e0ca80
Gaurav Kumar Borad <gjain7764@gmail.com>
2025-12-12 13:33:23 +0530

    this is my leetcode question solution

--- leetcode2.cpp
+++ leetcode2.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include <vector>
 using namespace std;
 void printArray(int arr[] , int size) {
     for (int i=0; i<size; i++) {
@@ -23,6 +24,34 @@ int uniqueNumber(int arr[], int size) {
     return num;
 
 }
+//find the duplicate number in array
+int duplicate(int arr[], int size) {
+    int ans=0;
+    for (int i=0; i<size; i++) {
+        ans=ans^arr[i];
+    }
+    for (int i=1; i<size; i++) {
+        ans= ans^ i;
+    }
+    return ans;
+}
+//find the interception between two arrays
+vector<int> interception(int arr1[], int arr2[], int m, int n) {
+    vector<int> ans;
+    for (int i=0; i<m; i++) {
+        int element= arr1[i];
+        for (int j=0; j<n; j++) {
+            if (element==arr2[j]) {
+                ans.push_back(element);
+                arr2[j]=INT_MIN;
+                break;
+
+            }
+        }
+    }
+    return ans;
+}
+
 
 int main() {
     cout <<"enter the size of an array "<<endl;
