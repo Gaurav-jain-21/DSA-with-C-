f9a09240d35d6abbfbfdb25dd0cdb453fc05cdc9
Gaurav Kumar Borad <gjain7764@gmail.com>
2025-12-13 14:21:56 +0530

    the code of how to find the square root of a particular number using binary search

--- binary_search2.cpp
+++ binary_search2.cpp
@@ -75,6 +75,30 @@ int pivot(int arr[], int size) {
     }
     return s;
 }
+//find the square root of a particular number using binary search
+long long int binarySearch(int n){
+    int s=0;
+    int e= n;
+    long long int mid=s+(e-s)/2;
+    int ans=-1;
+    while(s<=e){
+        long long int square= mid*mid;
+        if(square==n){
+            return mid;
+        }
+        else if(square<n){
+            ans=mid;
+            s= mid+1;
+        }
+        else{
+            e= mid-1;
+        }
+        mid= s+(e-s)/2;
+
+    }
+    return ans;
+
+}
 int main() {
     // int arr[7]= {1,2,3,4,4,4,8};
     // cout<<firstocc(arr, 7,4)<<endl;
