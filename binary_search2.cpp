af9538239259a4910d70458d548487ff84180dec
Gaurav Kumar Borad <gjain7764@gmail.com>
2025-12-13 14:46:08 +0530

    this is new question solution

--- binary_search2.cpp
+++ binary_search2.cpp
@@ -99,6 +99,48 @@ long long int binarySearch(int n){
     return ans;
 
 }
+bool isPossible(int arr[] , int n,int m, int mid) {
+    int studentCount=1;
+    int pageSum=0;
+    for (int i=0; i<n; i++) {
+        if (pageSum + arr[i]<=mid) {
+            pageSum+=arr[i];
+        }
+        else {
+            studentCount++;
+            if (studentCount>m||arr[i]>mid) {
+                return false;
+            }
+
+            pageSum=arr[i];
+        }
+    }
+    return true;
+}
+//number of minimum page of books
+int allocateBooks(int arr[] ,int n, int m) {
+    int s=0;
+    int sum=0;
+    for (int i=0; i<n; i++) {
+        sum= sum+arr[i];
+
+    }
+    int ans=-1;
+    int e=sum;
+    int mid=s+(e-s)/2;
+    while (s<=e) {
+        if (isPossible(arr,n,m,mid)) {
+            ans= mid;
+            e=mid-1;
+        }
+        else {
+            s= mid+1;
+
+        }
+        mid=s+(e-s)/2;
+    }
+    return ans;
+}
 int main() {
     // int arr[7]= {1,2,3,4,4,4,8};
     // cout<<firstocc(arr, 7,4)<<endl;
