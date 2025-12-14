c3f8e675afb60e8c8f0fc9482a496bea4517aa1b
Gaurav Kumar Borad <gjain7764@gmail.com>
2025-12-14 15:41:02 +0530

    this is an algorithm of sorting

--- bubble_sort1.cpp
+++ bubble_sort1.cpp
@@ -14,9 +14,20 @@ void bubbleSort(int arr[], int size) {
         }
     }
 }
+// in desending order
+void desBubbleSort(int arr[], int size) {
+    for (int i=1; i<size; i++) {
+        for (int j=0; j<size-i; j++) {
+            if (arr[j]<arr[j+1]) {
+                swap(arr[j],arr[j+1]);
+            }
+        }
+    }
+}
 int main() {
     int arr[5]={6,3,2,8,1};
-    bubbleSort(arr, 5);
+    // bubbleSort(arr, 5);
+    desBubbleSort(arr,5);
     printArray(arr, 5);
 
 }
