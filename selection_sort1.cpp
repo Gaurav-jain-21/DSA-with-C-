c1dc8c03aca994688e4616eb6dcea0047485c2f2
Gaurav Kumar Borad <gjain7764@gmail.com>
2025-12-14 15:17:04 +0530

    this is my selection sort

--- selection_sort1.cpp
+++ selection_sort1.cpp
@@ -17,9 +17,23 @@ void selection_sort(int arr[] , int size) {
         swap(arr[min],arr[i]);
     }
 }
+//arrnage the order into decending order
+
+void descSelection(int arr[], int size) {
+    for (int i=0; i<size-1;i++) {
+        int max= i;
+        for (int j =i+1; j<size; j++) {
+            if (arr[j]>arr[max]) {
+                max=j;
+            }
+        }
+        swap(arr[i],arr[max]);
+    }
+}
 int main() {
     int arr[5]={5,9,22,1,8};
-    selection_sort(arr,5);
+    // selection_sort(arr,5);
+    descSelection(arr,5);
     printArray(arr,5);
 
 }
