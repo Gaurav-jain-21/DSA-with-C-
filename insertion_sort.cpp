068018b33bf7fdea6937504146c2ad6e0a979f75
Gaurav Kumar Borad <gjain7764@gmail.com>
2025-12-15 13:38:22 +0530

    this is algorithm

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -1,5 +1,27 @@
 #include<iostream>
 using namespace std;
+void printArray(int arr[], int size) {
+    for (int i=0; i<size; i++) {
+        cout<<arr[i]<<" ";
+    }
+}
+//insertion sort
+void insertionSort(int arr[],int size) {
+    for (int i=1; i<size; i++) {
+        int temp=arr[i];
+        int j= i-1;
+        for (; j>=0; j--) {
+
+            if (arr[j]>temp) {
+                arr[j+1]=arr[j];
+            }
+            else {
+                break;
+            }
+        }
+        arr[j+1]=temp;
+    }
+}
 int main() {
     cout<<"hello world"<<endl;
 }
