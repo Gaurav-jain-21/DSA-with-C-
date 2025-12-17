2dee9fe9fa1f96e51699a881ac4493e92f61faa2
Gaurav Kumar Borad <gjain7764@gmail.com>
2025-12-17 23:38:16 +0530

    this is stl which is incomplete from my side right now but i will continue after some time

--- stl1.cpp
+++ stl1.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<array>
+#include<vector>
 using namespace std;
 int main() {
     int basic[3]={1,2,3};
@@ -10,4 +11,14 @@ int main() {
     }
 
     cout<<"Element at 2nd Index: "<<arr.at(2)<<endl;
+    cout<<"empty or not "<<arr.empty()<<endl;
+    cout<<"first element"<<arr.front()<<endl;
+    cout<<"last element"<<arr.back()<<endl;
+
+    //vectors
+    vector<int>v;
+    cout<<"size"<<v.capacity()<<endl;
+    v.push_back(1);
+    cout<<"size"<<v.capacity()<<endl;
+
 }
