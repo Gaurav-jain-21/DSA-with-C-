5829363567e33dcdbe9a301e616db4b94852f494
Gaurav Kumar Borad <gjain7764@gmail.com>
2025-12-30 00:41:46 +0530

    this is my pointer file

---
+++ Pointer1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int num=5;
+    cout<<num<<endl;
+    //address of operator -&
+    // cout<<"address of num is"<<&num<<endl;
+    int *ptr= &num;
+    cout<<*ptr<<endl;
+    double d= 4.3;
+    double *p2= &d;
+    cout<<"Address is : "<<p2<<endl;
+    cout<<*p2<<endl;
+    cout<<"size of integer is : "<<sizeof(num)<<endl;
+    cout<<"size of pointer is "<<sizeof(ptr)<<endl;
+    cout<<"size of pointer is : "<<sizeof(p2)<<endl;
+}
