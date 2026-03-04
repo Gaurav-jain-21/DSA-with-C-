5aa2cd98f4ffd0f059fe786dc5a65b3b91b8e26d
Gaurav Kumar Borad <gjain7764@gmail.com>
2026-03-04 13:49:02 +0530

    this is my 2d search

--- D2Array.cpp
+++ D2Array.cpp
@@ -1,5 +1,16 @@
 #include<iostream>
 using namespace std;
+bool isPresent(int arr[][4], int target ,int row, int col) {
+     for (int i=0; i<row; i++) {
+          for (int j=0; j<col; j++) {
+               if (arr[i][j] == target) {
+                    return true;
+               }
+          }
+     }
+     return false;
+
+}
 int main() {
      //create 2 d array
      int arr[3][4];
@@ -14,5 +25,14 @@ int main() {
                cout<<arr[i][j] <<" ";
           }
           cout<<endl;
+     }
+     cout<<"Enter the element to search";
+     int target;
+     cin>>target;
+     if (isPresent(arr, target, 3,4)) {
+          cout<<"Element Found";
+     }
+     else {
+          cout<<"Element Not Found";
      }
 }
