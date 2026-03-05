6561ae03ec25b9567cdc00ad5f8b5666bf305163
Gaurav Kumar Borad <gjain7764@gmail.com>
2026-03-05 07:07:26 +0530

    this is my commit

--- D2Array.cpp
+++ D2Array.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
-bool isPresent(int arr[][4], int target ,int row, int col) {
+bool isPresent(int arr[][3], int target ,int row, int col) {
      for (int i=0; i<row; i++) {
           for (int j=0; j<col; j++) {
                if (arr[i][j] == target) {
@@ -11,7 +11,7 @@ bool isPresent(int arr[][4], int target ,int row, int col) {
      return false;
 
 }
-void RowSum(int arr[][4],int row, int col) {
+void RowSum(int arr[][3],int row, int col) {
      int sum=0;
      for (int i=0; i<row;i++) {
           for (int j=0; j<col; j++) {
@@ -21,22 +21,40 @@ void RowSum(int arr[][4],int row, int col) {
           sum=0;
      }
 }
+int largestRowSum(int arr[][3], int row, int col) {
+     int max= INT_MIN;
+     int num=-1;
+     for (int row=0; row<row; row++) {
+          int sum=0;
+          for (int col=0; col<col; col++) {
+               sum+=arr[row][col];
+          }
+          if (sum>max) {
+
+               max=sum;
+               num= row;
+          }
+     }
+     cout<<"the maximum sum is "<<max <<endl;
+     return num;
+}
 int main() {
      //create 2 d array
-     int arr[3][4];
+     int arr[3][3];
      for (int i=0; i<3; i++) {
-          for (int j= 0; j<4; j++) {
+          for (int j= 0; j<3; j++) {
                cin>>arr[i][j];
           }
      }
      //print
      for (int i=0; i<3; i++) {
-          for (int j= 0; j<4; j++) {
+          for (int j= 0; j<3; j++) {
                cout<<arr[i][j] <<" ";
           }
           cout<<endl;
      }
-     RowSum(arr, 3 ,4);
+     RowSum(arr, 3 ,3);
+     cout<<largestRowSum(arr, 3,3);
      cout<<"Enter the element to search";
      int target;
      cin>>target;
